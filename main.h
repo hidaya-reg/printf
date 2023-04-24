aa259fae754939929f783eab12b6df746cb35849
hidaya-reg <hidaya.regragui@gmail.com>
2023-04-24 17:36:03 +0100

    printf

--- main.h
+++ main.h
@@ -1,28 +1,20 @@
-#ifndef _MAIN_H_
-#define _MAIN_H_
+#ifndef MAIN_H
+#define MAIN_H
 
 #include <stdarg.h>
+#include <stdlib.h>
 
-
-/**
- * struct fmt_fct - Structure to map format with appropriate function
- * to print it
- * @fmt: format
- * @fct: function to print @format
- */
-
-struct fmt_fct
-{
-	char fmt;
-	int (*fct)(va_list);
-};
 
 /**
  * typrdef struct fmt_fct fmt_t - define new type of struct fmt_fct
  * @fmt_fct: struct fmt_fct
  * @fmt_t: typedef fmt_fct
  */
-typedef struct fmt_fct fmt_t;
+typedef struct fmt_fct
+{
+	char *fmt;
+	int (*fct)();
+} fmt_t;
 
 int _putchar(char c);
 int _printf(const char *format, ...);
