e150290e43035695e679b9b5eae120e6bdd499ec
aminera <aminera03@gmail.com>
2023-04-25 13:37:16 -0700

    printf after betty check

--- main.c
+++ main.c
@@ -9,17 +9,16 @@
 int main(void)
 {
 	int len, len2;
+
 	len = _printf("Let's try to printf a simple sentence.\n");
 	len2 = printf("Let's try to printf a simple sentence.\n");
 	_printf("Length:[%d, %i]\n", len, len);
 	printf("Length:[%d, %i]\n", len2, len2);
-
 	_printf("Character:[%c]\n", 'H');
 	printf("Character:[%c]\n", 'H');
 	_printf("String:[%s]\n", "I am a string !");
 	printf("String:[%s]\n", "I am a string !");
 	_printf("Negative:[%d]\n", -762534);
 	printf("Negative:[%d]\n", -762534);
-	
 	return (0);
 }
