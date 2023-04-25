8cc345ee9ada46a7fc0e164f3b2ca165f523872c
aminera <aminera03@gmail.com>
2023-04-24 18:09:33 -0700

    printfa

--- main.h
+++ main.h
@@ -5,6 +5,8 @@
 #include <stdlib.h>
 #include <unistd.h>
 
+#define UNUSED(x) (void)(x)
+#define BUFF_SIZE 1024
 
 /**
  * typrdef struct fmt_fct fmt_t - define new type of struct fmt_fct
@@ -23,5 +25,28 @@ int print_str(va_list args);
 int print_char(va_list args);
 int print_pct(void);
 
+/****************** FUNCTIONS ******************/
+
+/* width handler */
+int handle_write_char(char c, char buffer[],
+	int flags, int width, int precision, int size);
+int write_number(int is_positive, int ind, char buffer[],
+	int flags, int width, int precision, int size);
+int write_num(int ind, char bff[], int flags, int width, int precision,
+	int length, char padd, char extra_c);
+int write_pointer(char buffer[], int ind, int length,
+	int width, int flags, char padd, char extra_c, int padd_start);
+
+int write_unsgnd(int is_negative, int ind,
+char buffer[],
+	int flags, int width, int precision, int size);
+
+/****************** UTILS ******************/
+int is_printable(char);
+int append_hexa_code(char, char[], int);
+int is_digit(char);
+
+long int convert_size_number(long int num, int size);
+long int convert_size_unsgnd(unsigned long int num, int size);
 
 #endif
